f3505578e7306bc1024f98afe093bdfd28cef34a
Bonobo King <aammiitt11001100@gmail.com>
2022-02-26 00:52:31 +0200

    finished course_information!

---
+++ part1/course_information/src/App.jsx
@@ -0,0 +1,51 @@
+const Header = ({ course }) => { return <h1>{course}</h1> }
+const Part = ({ partName, exercisesCount }) => {
+  return (<p>{partName} {exercisesCount}</p>)
+}
+const Content = ({ partArray }) => {
+  let ret = []
+  // partArray.forEach(part => {
+  //   ret.push(<Part partName={part.name} exercisesCount={part.exercises} />)
+  // });
+  for (let i = 0; i < partArray.length; i++) {
+    ret.push(<Part key={i} partName={partArray[i].name} exercisesCount={partArray[i].exercises} />)
+  }
+  return ret
+}
+const Total = ({ partArray }) => {
+  // for readability
+  let totalExercises = 0
+  partArray.forEach(part => {
+    totalExercises += part.exercises
+  })
+  // or like this
+  let tots = partArray.reduce((curr, next) => ({ exercises: curr.exercises + next.exercises }))['exercises']
+  // cause im a fan of oneliners
+  return <p>Number of exercises {totalExercises}</p>
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  return (
+    <div>
+      <Header course={course} />
+      <Content partArray={parts} />
+      <Total partArray={parts} />
+    </div>
+  )
+}
+
+export default App
